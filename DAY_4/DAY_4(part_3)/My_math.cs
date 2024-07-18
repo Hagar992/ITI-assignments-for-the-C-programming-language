3b4be6268e0ac7b44140ab6d5a852d953f2132f8
unknown <shamsatia96@gmail.com>
2024-07-18 23:06:03 +0300

    Add file

---
+++ DAY_4/DAY_4(part_3)/My_math.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAY_4_part_3_
+{
+    internal class My_math
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Mul(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Div(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Division by zero is not allowed.");
+            }
+            return a / b;
+        }
+
+        public int Reminder(int a, int b)
+        {
+            return a % b;
+        }
+    }
+}
---
+++ DAY_4/DAY_4(part_3)/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAY_4_part_3_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            My_math math = new My_math();
+
+            Console.WriteLine("Enter the first number:");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number:");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Calculator:");
+            Console.WriteLine($"Add {num1} + {num2} = " + math.Add(num1, num2));
+            Console.WriteLine($"Sub {num1} - {num2} = " + math.Sub(num1, num2));
+            Console.WriteLine($"Mul {num1} * {num2} = " + math.Mul(num1, num2));
+            Console.WriteLine($"Div {num1} / {num2} = " + (num2 != 0 ? math.Div(num1, num2).ToString() : "Cannot divide by zero"));
+            Console.WriteLine($"Reminder {num1} % {num2} = " + (num2 != 0 ? math.Reminder(num1, num2).ToString() : "Cannot divide by zero"));
+        }
+
+
+    }
+
+}
